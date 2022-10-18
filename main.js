bd6d5c44b12246f4f12acc14c9902ed3842c57c0
Josh Koter <josh.koter@gmail.com>
2022-10-18 22:55:50 +0200

    Added skull image that follows users mouse position

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-let hamburger = document.querySelector('nav span')
-let nav = document.querySelector('nav')
+window.addEventListener('load', () => {
+  const hamburger = document.querySelector('nav span'),
+    nav = document.querySelector('nav'),
+    leftEyeball = document.querySelector('#eye-left'),
+    rightEyeball = document.querySelector('#eye-right'),
+    skull = document.querySelector('#skull'),
+    eyes = document.querySelectorAll('.eye')
 
-hamburger.addEventListener('click', () => {
-  nav.classList.toggle('active')
+  // toggle menu on mobile
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active')
+  })
+
+  // move eyeballs on mousemove
+  window.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX,
+      mouseY = e.clientY,
+      rect = skull.getBoundingClientRect(),
+      anchorX = rect.left + rect.width / 2,
+      anchorY = rect.top + rect.height / 2,
+      angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
+    eyes.forEach((eye) => {
+      eye.style.transform = `rotate(${90 + angleDeg}deg)`
+    })
+    console.log(mouseY)
+  })
+
+  // gets angle value
+  function angle(cx, cy, ex, ey) {
+    const dy = ey - cy,
+      dx = ex - cx,
+      rad = Math.atan2(dy, dx), // range (-PI, PI)
+      deg = (rad * 180) / Math.PI // rads to degrees, range (-180, 180)
+    return deg
+  }
 })
