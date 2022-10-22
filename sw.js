3fcbf1fe5f62794a30a280c72b3c047766104f4e
Josh Koter <josh.koter@gmail.com>
2022-10-22 09:40:06 +0200

    PWA - full cached audio visualiser project

--- sw.js
+++ sw.js
@@ -2,7 +2,15 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     // static is an appropriate name for a cache name
     caches.open('static').then((cache) => {
-      return cache.addAll(['./', './style.css', './images/skull.png'])
+      return cache.addAll([
+        './',
+        './style.css',
+        './images/skull.png',
+        './audio-visualiser/favicon.svg',
+        './audio-visualiser/index.html',
+        './audio-visualiser/script.js',
+        './audio-visualiser/style.css'
+      ])
     })
   )
 })
